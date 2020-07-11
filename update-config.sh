06b24269bf9dd1d76edc2025d3a7d1879f7437bb
Oleksii Troian <trolleksii@gmail.com>
2020-07-11 16:04:53 -0400

    release5

--- update-config.sh
+++ update-config.sh
@@ -1,24 +1,19 @@
-git config --global user.email "cbuild@acme.com"
-git config --global user.name "Google Cloud Build"
-# echo -n "https://$_GITHUB_USER:$_GITHUB_PASSWORD@github.com/$_GITHUB_USER/gitops-demo-infra.git" > /workspace/.git-credentials
 git clone https://$_GITHUB_USER:$_GITHUB_PASSWORD@github.com/trolleksii/gitops-demo-infra.git --branch dev
+git config --global user.name "Google Cloud Build"
+git config --global user.email "cbuild@acme.com"
 cd gitops-demo-infra
-# git config --global credential.helper 'store --file /workspace/.git-credentials'
-git fetch --all --tags
-git tag -l
-export TAG_NAME="$(git describe --tags)"
 # update config
 # imitating image tag variable substitution
-sed -i "s/^app = .*$/app = \"$TAG_NAME\"/g" terraform/main.tfvars
+sed -i "s/^app = .*$/app = \"$(cat /repo-tag/tag)\"/g" terraform/main.tfvars
 git add terraform/main.tfvars
 cat <<EOF | git commit -F-
 Update the gitops-demo-app
 
 This commit updates the container image to:
-    gcr.io/$PROJECT_ID/$_APPLICATION_NAME:$TAG_NAME.
+    gcr.io/$PROJECT_ID/$_APPLICATION_NAME:$(cat /repo-tag/tag).
 
 Build ID: $BUILD_ID
 EOF
-git tag $TAG_NAME
+git tag $(cat /repo-tag/tag)
 git push --tags
 git push origin dev
