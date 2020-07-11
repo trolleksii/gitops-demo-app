72b84b17b04df5588679b70997f1342f80cb4192
Oleksii Troian <trolleksii@gmail.com>
2020-07-11 15:58:55 -0400

    release5

--- update-config.sh
+++ update-config.sh
@@ -1,10 +1,9 @@
 git config --global user.email "cbuild@acme.com"
 git config --global user.name "Google Cloud Build"
 # echo -n "https://$_GITHUB_USER:$_GITHUB_PASSWORD@github.com/$_GITHUB_USER/gitops-demo-infra.git" > /workspace/.git-credentials
-git clone https://$_GITHUB_USER:$_GITHUB_PASSWORD@github.com/trolleksii/gitops-demo-infra.git
+git clone https://$_GITHUB_USER:$_GITHUB_PASSWORD@github.com/trolleksii/gitops-demo-infra.git --branch dev
 cd gitops-demo-infra
 # git config --global credential.helper 'store --file /workspace/.git-credentials'
-git checkout dev
 git fetch --all --tags
 git tag -l
 export TAG_NAME="$(git describe --tags)"
