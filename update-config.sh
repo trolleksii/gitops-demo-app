339be3f963d5c64ae8a461e4ef2d02034ff544d8
Oleksii Troian <trolleksii@gmail.com>
2020-07-11 16:47:05 -0400

    release5

--- update-config.sh
+++ update-config.sh
@@ -4,16 +4,16 @@ git config --global user.email "cbuild@acme.com"
 cd gitops-demo-infra
 # update config
 # imitating image tag variable substitution
-sed -i "s/^app = .*$/app = \"$(cat /repo-tag/tag)\"/g" terraform/main.tfvars
+sed -i "s/^app = .*$/app = \"$(cat /workspace/tag)\"/g" terraform/main.tfvars
 git add terraform/main.tfvars
 cat <<EOF | git commit -F-
 Update the gitops-demo-app
 
 This commit updates the container image to:
-    gcr.io/$PROJECT_ID/$_APPLICATION_NAME:$(cat /repo-tag/tag).
+    gcr.io/$PROJECT_ID/$_APPLICATION_NAME:$(cat /workspace/tag).
 
 Build ID: $BUILD_ID
 EOF
-git tag $(cat /repo-tag/tag)
+git tag $(cat /workspace/tag)
 git push --tags
 git push origin dev
