76fb9f6136379dc6920402853c3b664f45d5c685
Oleksii Troian <trolleksii@gmail.com>
2020-07-11 17:02:56 -0400

    release6

--- update-config.sh
+++ update-config.sh
@@ -1,4 +1,4 @@
-git clone https://$_GITHUB_USER:$_GITHUB_PASSWORD@github.com/trolleksii/gitops-demo-infra.git --branch dev
+git clone https://${_GITHUB_USER}:${_GITHUB_PASSWORD}@github.com/trolleksii/gitops-demo-infra.git --branch dev
 cd gitops-demo-infra
 git config --global user.name "Google Cloud Build"
 git config --global user.email "cbuild@acme.com"
@@ -10,9 +10,9 @@ cat <<EOF | git commit -F-
 Update the gitops-demo-app
 
 This commit updates the container image to:
-    gcr.io/$PROJECT_ID/$_APPLICATION_NAME:$(cat /workspace/tag).
+    gcr.io/${PROJECT_ID}/${_APPLICATION_NAME}:$(cat /workspace/tag).
 
-Build ID: $BUILD_ID
+Build ID: ${BUILD_ID}
 EOF
 git remote get-url origin
 git push
